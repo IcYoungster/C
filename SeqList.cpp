506e4fe9de2ec1424c8243965237e9a3a381d66e
IcYoungster <icyoung2020@163.com>
2023-08-12 17:23:34 +0800

    Add the way of Dynamic arrange space for SeqList. Compelete the fuction of Initial and Increase Print.

--- SeqList.cpp
+++ SeqList.cpp
@@ -1,10 +1,53 @@
 #include<stdio.h>
+#include<stdlib.h>
 #define MAXSIZE 50
 typedef int ElemType;
 typedef struct{
     ElemType data[MAXSIZE];
     int length;
 }SqList;
+
+//the upper one was good, but as we reached the MAXSIZE,we could never fill a single data into it
+//but Dynamic SeqList, which allows us to add the maxsize of list freely
+#define InitSize 10//define the dynamic list first size of data
+typedef struct{
+    ElemType *data;//this pointer pointed to the first data of the list, aka address
+    int MaxSize;
+    int length;//record recent length
+}DySeqList;
+//so how to apply for a space dynamically?
+//C language use the key word "malloc" to apply and the "free" to give space back
+
+void DyInitList(DySeqList &DL)
+{
+    //malloc give back a pointer,so we need to trans with force
+    //the space we need is euqals to = size(number) * size of type
+    DL.data = (ElemType*)malloc(InitSize*sizeof(ElemType));
+    DL.length = 0;
+    DL.MaxSize = InitSize;
+}
+
+//add a function to increase the lenth of Dynamic Seqlist
+//main idea is to create a new list with extra length,
+//then swap all the data to the new list, delete the old one in the end
+//followed with few steps:
+//1.define a pointer pointed to DL.data,this pointer is a temporary container
+//2.apply more space for "DL.data" List
+//3.swap data one by one from that temp pointer to our new "DL.data"
+//4.change the params like length and maxsize.
+//5.free temp pointer,which actually freed the space pointed to
+void IncreaseSize(DySeqList &DL,int length)
+{
+    ElemType *TempPointer = DL.data;
+    DL.data = (ElemType*)malloc((InitSize+length)*(sizeof(ElemType)));
+    for(int i=0;i <= InitSize;i++)
+    {
+        DL.data[i] = TempPointer[i];
+    }
+    DL.length += length;
+    DL.MaxSize += length;
+    free(TempPointer);
+}
 
 void InitList(SqList &L)
 {
@@ -22,6 +65,14 @@ void PrintList(SqList L)
     for(;i<L.length;i++)
     {
         printf("%-3d",L.data[i]);
+    }
+}
+
+void PrintDyList(DySeqList DL)
+{
+    for(int i=0;i<DL.length;i++)
+    {
+        printf("%-3d",DL.data[i]);
     }
 }
 
@@ -83,13 +134,23 @@ ElemType FindByNum(SqList L,int location)
 
 int main()
 {
+    //SqList L1 = {{1,1,1},3}; could like this,but don't initial
     SqList L1;
+    DySeqList L2;
     InitList(L1);
+    DyInitList(L2);
     L1.length = 3;
+    L2.length = 3;
     L1.data[0] = 1;
-    L1.data[1] = 2;
-    L1.data[2] = 3;
-    //FindByValue(L1,2);
-    int ret = FindByNum(L1,1);
-    printf("%d",ret);
+    L1.data[1] = 1;
+    L1.data[2] = 1;
+    L2.data[0] = 9;
+    L2.data[1] = 9;
+    L2.data[2] = 9;
+    PrintList(L1);
+    printf("\n");
+    PrintDyList(L2);
+    printf("\n");
+    IncreaseSize(L2,3);
+    PrintDyList(L2);
 }
