497fc900fe79031d27312a66b40dad60f9928986
IcYoungster <icyoung2020@163.com>
2023-10-17 20:31:30 +0800

    Complete the Head&Rear Insert function

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -6,6 +6,7 @@ typedef struct LNode
     ElemType data;
     struct LNode *Next;
 } LNode, *LinkedList;
+
 bool InitLinkedList(LinkedList &List)
 {
     List = (LNode *)malloc(sizeof(LNode));
@@ -17,12 +18,8 @@ bool InitLinkedList(LinkedList &List)
     return true;
 }
 
-//Print The LinkedList
 void PrintList(LinkedList List)
 {
-    //Create a Pointer to Traversal the list
-    //Give the HeadNode's address to Pointer
-    //Then Print every Node's data,quit when the "Node->Next" is NULL
     LNode* P;
     P = List->Next;
     while (P != NULL)
@@ -32,21 +29,8 @@ void PrintList(LinkedList List)
     }
 }
 
-// How to Insert a NewNode into our LinkedList?
-// if we already got a List with 3 Nodes,now we want insert one NewNode as second Node,
-// The params will be (1.List 2.Data 3.Insert Position)->(L,D,2)
-// p.s. The Node will be create inside the function
-
-// so in this function needs two steps:
-// 1.Find the position
-// 2.Insert NewNode
 bool Insert(LinkedList &List, ElemType NewData, int position)
 {
-    // Step1:Find the position,we need to Traversal the list
-    // 1.Create a temp pointer for locate & get address of Node
-    // 2.Give the address of HeadNode to TempPointer
-    // 3.Use index to show where the TempPointer is, also, as a index of loop
-    // 4.Traversal the List
     int p = position;
     int i = 0;
     LNode *TempPointer;
@@ -58,12 +42,6 @@ bool Insert(LinkedList &List, ElemType NewData, int position)
         i++;
     }
 
-    // Step2:Insert NewNode
-    // 1.Create a NewNode
-    // 2.Give NewData to NewNode's data
-    // 3.Give Next Nodes' address to NewNode's next
-    //(Now NewNode's next& Pointer's next = NextNode's address)
-    // 4.Give NewNode's address to Pointer's next
     LNode *NewNode = (LNode *)malloc(sizeof(LNode));
     NewNode->data = NewData;
     NewNode->Next = TempPointer->Next;
@@ -71,18 +49,82 @@ bool Insert(LinkedList &List, ElemType NewData, int position)
 
     return true;
 }
+
+// define a function to build LinkedList from scratch,
+// it conprised with initial and add new data Node,
+// which would help us when there are plenties of Data
+
+// There are two different ways to insert elements in the Linked List
+// 1.Insert new Node after Head Node of the list, name the function as HeadInsert
+    // To add a new Node after the HeadNode like this:
+    // HeadPointer -> HeadNode -> NULL
+    // HeadPointer -> HeadNode ->New Node 1 ->NULL
+    // HeadPointer -> HeadNode ->New Node 2 ->New Node 1 ->NULL
+// 2.Insert new Node after the last Node of the List,name it RearInsert
+    // HeadPointer -> HeadNode -> NULL
+    // HeadPointer -> HeadNode ->New Node 1 ->NULL
+    // HeadPointer -> HeadNode ->New Node 1 ->New Node 2 ->NULL
+
+LinkedList HeadInsert(LinkedList &List)
+{
+    // First we Create the LinkedList with HeadNode,Same with the "InitLinkedList"
+    // Then Set a Number, Control the Loop, Insert the Data
+    List = (LNode *)malloc(sizeof(LNode));
+    List->Next = NULL;
+
+    ElemType i = NULL;
+    LNode* NewNode;
+    scanf("%d",&i);
+    while (i != 9999)
+    {
+        NewNode = (LNode*)malloc(sizeof(LNode));
+        NewNode->data = i;
+        NewNode->Next = List->Next;
+        List->Next = NewNode;
+        scanf("%d",&i);
+    }
+    return List;
+}
+
+//Pretty much same with HeadInsert, but add a RearNode
+LinkedList RearInsert(LinkedList &List)
+{
+    List = (LNode *)malloc(sizeof(LNode));
+    List->Next = NULL;
+
+    ElemType i = NULL;
+    
+    LNode* NewNode;
+    LNode* RearNode = List;//Pointed to the Rear of List
+    scanf("%d",&i);
+    while (i != 9999)
+    {
+        // Create a NewNode,Then Insert it at the end
+        // At first,RearNode & List all pointed to HeadNode,
+        // so RearNode->Next == HeadNode->Next
+        // Then let RearNode pointed to NewNode
+        NewNode = (LNode*)malloc(sizeof(LNode));
+        NewNode->data = i;
+        RearNode->Next = NewNode;
+        RearNode = NewNode;
+        scanf("%d",&i);
+    }
+    return List;
+}
+
 int main()
 {
     // At first,we did't create a Node but only a pointer
-    LinkedList L; // Or LNode* L;
-
+    LinkedList L1; // Or LNode* L;
+    LinkedList L2;
     // Now Initialized it,we create a Head Node for convinience
-    InitLinkedList(L);
+    //InitLinkedList(L);
 
     // Then we can fill The List with data
-    Insert(L, 666, 1);
-    Insert(L, 2, 2);
-    Insert(L, 3, 3);
-    PrintList(L);
+    // But insert Data on by one is STUPID!
+    HeadInsert(L1);
+    RearInsert(L2);
+    PrintList(L1);
+    PrintList(L2);
     return 0;
 }
